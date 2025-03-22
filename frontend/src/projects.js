a27ee1c996147aca78c9281578db0d02b02ccf8f
disruptiboy <luis.enhe96@gmail.com>
2025-03-22 13:16:23 +0100

    Create projects.js

---
+++ frontend/src/projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div className="projects-container">
+      <h2>Proyectos de Reforma</h2>
+      <div className="gallery">
+        <img src="/images/project1.jpg" alt="Proyecto 1" />
+        <img src="/images/project2.jpg" alt="Proyecto 2" />
+        <img src="/images/project3.jpg" alt="Proyecto 3" />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
